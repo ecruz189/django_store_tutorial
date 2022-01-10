9743e4b1c392df7f32553e5f745b6245a231a4b6
ecruz189 <76583011+ecruz189@users.noreply.github.com>
2022-01-10 21:22:35 +1000

    Update settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*gb=)31kt0od_2p)dc3%!@ltmqjlq&__u3v*1@q%etg_vf8#)a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
